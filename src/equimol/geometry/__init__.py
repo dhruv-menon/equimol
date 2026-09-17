02076b11feb89c608c287455b8a8473b5a1c2f8c
Dhruv Menon <128166706+dhruv-menon@users.noreply.github.com>
2026-09-17 14:43:04 +0100

    Updated geometry/__init__.py

--- src/equimol/geometry/__init__.py
+++ src/equimol/geometry/__init__.py
@@ -1,6 +1,6 @@
 """Geometric primitives for molecular and protein structure."""
 
-from .angles import bond_angle
+from .angles import bond_angle, bond_angle_features_from_index, bond_angles_from_index
 from .backbone import (
     backbone_bond_angles,
     backbone_bond_lengths,
@@ -9,16 +9,24 @@ from .backbone import (
 )
 from .distances import distance, squared_distance
 from .indexing import angle_triplets_from_edges, edge_pairs, torsion_quartets_from_edges
-from .torsions import dihedral_angle
+from .torsions import (
+    dihedral_angle,
+    dihedral_angles_from_index,
+    dihedral_features_from_index,
+)
 
 __all__ = [
     "angle_triplets_from_edges",
     "bond_angle",
+    "bond_angle_features_from_index",
+    "bond_angles_from_index",
     "backbone_bond_angles",
     "backbone_bond_lengths",
     "backbone_geometry",
     "backbone_torsions",
     "dihedral_angle",
+    "dihedral_angles_from_index",
+    "dihedral_features_from_index",
     "distance",
     "edge_pairs",
     "squared_distance",
