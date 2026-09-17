fe4e5af5b4effc1a7eeedd9af93aad384ab590e4
Dhruv Menon <128166706+dhruv-menon@users.noreply.github.com>
2026-09-17 14:50:31 +0100

    Updated features/__init__.py

--- src/equimol/features/__init__.py
+++ src/equimol/features/__init__.py
@@ -1,17 +1,23 @@
 """Feature construction utilities for molecular and protein ML."""
 
-from .molecule import MolecularEdgeFeatures, MolecularNodeFeatures
-from .molecule import molecule_atom_features, molecule_edge_features
+from .molecule import (
+    MolecularEdgeFeatures,
+    MolecularGeometryFeatures,
+    MolecularNodeFeatures,
+)
+from .molecule import molecule_atom_features, molecule_edge_features, molecule_geometry_features
 from .protein import ProteinEdgeFeatures, ProteinNodeFeatures
 from .protein import protein_atom_features, protein_edge_features, protein_residue_features
 
 __all__ = [
     "MolecularEdgeFeatures",
+    "MolecularGeometryFeatures",
     "MolecularNodeFeatures",
     "ProteinEdgeFeatures",
     "ProteinNodeFeatures",
     "molecule_atom_features",
     "molecule_edge_features",
+    "molecule_geometry_features",
     "protein_atom_features",
     "protein_edge_features",
     "protein_residue_features",
