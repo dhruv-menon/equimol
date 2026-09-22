ef1c6021facdbf163d8e914a12111a4beaabec63
Dhruv Menon <128166706+dhruv-menon@users.noreply.github.com>
2026-09-22 10:33:08 +0100

    Implemented tests on the vectoregnn

--- tests/test_vector_egnn.py
+++ tests/test_vector_egnn.py
@@ -1,9 +1,11 @@
 import torch
+import pytest
 
 from equimol.models import VectorEGNNRegressor
 
 
-def test_vector_egnn_regressor_forward_and_force_shapes():
+@pytest.mark.parametrize("vector_gate", [False, True])
+def test_vector_egnn_regressor_forward_and_force_shapes(vector_gate):
     num_nodes, num_edges = 6, 12
     node_feat_dim, edge_attr_dim = 10, 5
 
@@ -20,6 +22,7 @@ def test_vector_egnn_regressor_forward_and_force_shapes():
         edge_attr_dim=edge_attr_dim,
         message_dim=12,
         vector_dim=8,
+        vector_gate=vector_gate,
     )
 
     energy = model(h, x, edge_index, batch, edge_attr)
